a530bdcbe7d3ec5b5ac8c2c746e906a819be8edd
kmlongchun <kimlong210806@gmail.com>
2026-05-23 23:15:04 +0700

    Update data_hr.py

--- data_hr.py
+++ data_hr.py
@@ -11,7 +11,7 @@ conn = mysql.connector.connect(
     port=3306,
     user="root",
     password="root",
-    database="quanlynhansucongviec"  # ← tên DB của team
+    database="quanlynhansucongviec"  
 )
 cursor = conn.cursor()
 
